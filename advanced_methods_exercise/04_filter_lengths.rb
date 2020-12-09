33fa2b3a47c1520e879de08bfca29babf090beea
addislw <addislw@gmail.com>
2020-12-08 22:40:32 -0500

    Method created that filters array of strings based on length

--- advanced_methods_exercise/04_filter_lengths.rb
+++ advanced_methods_exercise/04_filter_lengths.rb
@@ -3,6 +3,9 @@
 # that have at least the given length. The length argument should be optional; if no length
 # is passed in, then 5 should be used as the length.
 
+def filter_lengths(string_arr, length = 5)
+  string_arr.select { |string| string.length >= length }
+end
 
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
