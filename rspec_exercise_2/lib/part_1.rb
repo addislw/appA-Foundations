b6c97d2db480595f9719e5064ae1b245d85f601d
addislw <addislw@gmail.com>
2020-12-16 01:28:52 -0500

    Method substrings accepts a string argument and returns an array of all possible substrings

--- rspec_exercise_2/lib/part_1.rb
+++ rspec_exercise_2/lib/part_1.rb
@@ -9,3 +9,16 @@ def palindrome?(string)
 
   reverse_string == string
 end
+
+
+
+def substrings(string)
+  subs = []
+  (0...string.length).each do |start_idx|
+    (start_idx...string.length).each do |end_idx|
+      subs << string[start_idx..end_idx]
+    end
+  end
+    
+  subs
+end
