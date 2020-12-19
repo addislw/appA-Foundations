689d94a59fe5b2f58bae62133210790c54638c1e
addislw <addislw@gmail.com>
2020-12-18 20:09:04 -0500

    Method created to accept array of urls and check if any contain a valid domain

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -1,3 +1,10 @@
 def all_words_capitalized?(word_array)
   word_array.all? { |word| word == word.capitalize }
 end
+
+
+def no_valid_url?(url_array)
+  domains = ['.com', '.net', '.io', '.org']
+  url_array.none? { |url| url.end_with?(*domains) }
+end
+
