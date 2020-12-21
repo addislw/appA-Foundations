1894b04f91055e83bad32f14571a7c1d252df628
addislw <addislw@gmail.com>
2020-12-21 17:04:00 -0500

    Dog class initialized with required attributes and getters created using attr_reader

--- dog_project/lib/dog.rb
+++ dog_project/lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+  
+  attr_reader :name, :breed, :age, :favorite_foods
 
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
+
+
+  
 end
