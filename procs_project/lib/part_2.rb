9fef2a2373d395e5586dde883a2ecd9873a7a0be
addislw <addislw@gmail.com>
2020-12-21 14:50:18 -0500

    Method created that accepts an array and two procs, mapping each element to a new array after performing function of one proc, based on parity of element's index

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -20,3 +20,28 @@ def greater_proc_value(num, prc_1, prc_2)
     return result_2
   end
 end
+
+
+
+def and_selector(array, prc_1, prc_2)
+  new_array = array.select do |ele|
+    prc_1.call(ele) && prc_2.call(ele)
+  end
+
+  new_array
+end
+
+
+
+def alternating_mapper(array, prc_1, prc_2)
+  new_array = array.map.with_index do |ele, idx|
+    if idx % 2 == 0 
+      prc_1.call(ele)
+    else
+      prc_2.call(ele)
+    end
+  end
+
+  new_array
+end
+
