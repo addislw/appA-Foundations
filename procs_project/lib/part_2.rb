367e863c86415ce4cf4c9ee68749068a98af751b
addislw <addislw@gmail.com>
2020-12-21 11:35:55 -0500

    Method created that passes each word of a sentence into a block and returns the modified words as a new sentence

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -1,3 +1,11 @@
 def reverser(string, &prc)
   prc.call(string.reverse)
 end
+
+
+def word_changer(string, &prc)
+  new_sentence = string.split.map do |word|
+    prc.call(word)
+  end
+  new_sentence.join(' ')
+end
