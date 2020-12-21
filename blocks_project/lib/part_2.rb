551457e00e601c3043dbaab24cbea48c50a2b5e6
addislw <addislw@gmail.com>
2020-12-20 23:51:03 -0500

    Method created that determines whether any element in a hash array has a minimum value for a specific key

--- blocks_project/lib/part_2.rb
+++ blocks_project/lib/part_2.rb
@@ -8,3 +8,10 @@ def no_valid_url?(url_array)
   url_array.none? { |url| url.end_with?(*domains) }
 end
 
+
+def any_passing_students?(hash_array)
+  hash_array.any? do |hash|
+    avg_grade = hash[:grades].sum / hash[:grades].length
+    avg_grade >= 75
+  end
+end
