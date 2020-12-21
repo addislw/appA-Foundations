d0ee6581ad559f7b01f669c370b5382ae1839cb6
addislw <addislw@gmail.com>
2020-12-21 11:43:59 -0500

    Method created that takes a number created along with 2 procs, returning the greater of outputs when number is passed into each proc

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -9,3 +9,14 @@ def word_changer(string, &prc)
   end
   new_sentence.join(' ')
 end
+
+
+def greater_proc_value(num, prc_1, prc_2)
+  result_1 = prc_1.call(num)
+  result_2 = prc_2.call(num)
+  if result_1 > result_2
+    return result_1
+  else
+    return result_2
+  end
+end
